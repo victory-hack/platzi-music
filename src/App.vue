944a86b967f2fa46f8b1ed2cf30287434aecd59b
Victor Cañete <victorcanete@Victors-MacBook-Pro.local>
2021-12-07 19:18:19 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  #app
+    body
+      .container
+          .container-img
+              img(src="./assets/img.jpg" class="container-img--responsive")
+          h2(class="title") Registro estación Platzi
+
+          .container-element
+              label(for="name" class="container-label") Nombre
+              input(type="text" id="name" name="name" class="container-input")
+          .container-element
+              label(for="lastname" class="container-label") Apellido
+              input(type="text" id="lastname" name="lastname" class="container-input")
+          .container-element
+              label(for="role" class="container-label") Cargo
+              input(type="text" id="role" name="role" class="container-input")
+              
+          .container-submit
+              button(class="container-btn") Enviar
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hola Vue!'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './scss/main.scss'
+</style>
---
+++ src/components/movies/movies.vue
@@ -0,0 +1,84 @@
+<template lang="pug">
+  #app
+    .header
+      nav
+        .logo
+          img.logo(src='./../../assets/pelis.jpg')
+        .search-bar
+          input.search(type='text' placeholder='Busca tu peli, actor, actriz ó director favorito')
+        .user
+          div.container
+            p.user-name Juan Luis Rojas
+            i.fa.fa-user-circle-o(aria-hidden='true')
+            i.fa.fa-sort-desc(aria-hidden='true')
+    .content
+      .side-bar
+        .first
+          i.fa.fa-home(aria-hidden='true')
+          p Home
+          i.fa.fa-heart(aria-hidden='true')
+          p Trending
+          i.fa.fa-fire(aria-hidden='true')
+          p More likes
+        .last
+          i.fa.fa-cog(aria-hidden='true')
+          p Settings
+      .content-down
+        h2 Ahora en cine
+        div.container-images
+          div.over
+            div.color
+              h4 The Secret Life of Pets
+              h6 The quiet life of a terrier named Max is upended when his owner takes in Duke, a stray whom Max instantly dislikes.
+          div.over
+            div.color
+              h4 The Secret Life of Pets
+              h6 The quiet life of a terrier named Max is upended when his owner takes in Duke, a stray whom Max instantly dislikes.
+          div.over
+            div.color
+              h4 The Secret Life of Pets
+              h6 The quiet life of a terrier named Max is upended when his owner takes in Duke, a stray whom Max instantly dislikes.
+        h2 Aclamadas por la crítica
+        div.container-images
+            div.over
+              div.color
+                h4 The Secret Life of Pets
+                h6 The quiet life of a terrier named Max is upended when his owner takes in Duke, a stray whom Max instantly dislikes.
+            div.over
+              div.color
+                h4 The Secret Life of Pets
+                h6 The quiet life of a terrier named Max is upended when his owner takes in Duke, a stray whom Max instantly dislikes.
+            div.over
+              div.color
+                h4 The Secret Life of Pets
+                h6 The quiet life of a terrier named Max is upended when his owner takes in Duke, a stray whom Max instantly dislikes.
+        h2 No te debes  de perder
+        div.container-images
+          div.over
+            div.color
+              h4 The Secret Life of Pets
+              h6 The quiet life of a terrier named Max is upended when his owner takes in Duke, a stray whom Max instantly dislikes.
+          div.over
+            div.color
+              h4 The Secret Life of Pets
+              h6 The quiet life of a terrier named Max is upended when his owner takes in Duke, a stray whom Max instantly dislikes.
+          div.over
+            div.color
+              h4 The Secret Life of Pets
+              h6 The quiet life of a terrier named Max is upended when his owner takes in Duke, a stray whom Max instantly dislikes.
+</template>
+
+<script>
+export default {
+  name: 'movie',
+  data () {
+    return {
+      msg: 'Hola Vue!'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './../../scss/main.scss'
+</style>
