4edeb9f58885eb6a9cf8ca4030e16d057893783d
victory-hack <dj.vic6@gmail.com>
2021-12-07 22:56:25 -0600

    Add: directives, watchers, computing, events and inputs

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,11 @@
 <template lang="pug">
   #app
-    body
-      .container
-          .container-img
-              img(src="./assets/img.jpg" class="container-img--responsive")
-          h2(class="title") Registro estación Platzi
+    input(v-model="name")
+    input(v-model="lastName")
+    h1 {{ fullName }}
+    button(v-on:click="format") Format
 
-          .container-element
-              label(for="name" class="container-label") Nombre
-              input(type="text" id="name" name="name" class="container-input")
-          .container-element
-              label(for="lastname" class="container-label") Apellido
-              input(type="text" id="lastname" name="lastname" class="container-input")
-          .container-element
-              label(for="role" class="container-label") Cargo
-              input(type="text" id="role" name="role" class="container-input")
-              
-          .container-submit
-              button(class="container-btn") Enviar
+    h1 {{ formatName }}
 </template>
 
 <script>
@@ -25,7 +13,24 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Hola Vue!'
+      name: '',
+      lastName: '',
+      formatName: ''
+    }
+  },
+  computed: {
+    fullName () {
+      return `${this.name} ${this.lastName}`
+    }
+  },
+  watch: {
+    name (newVal, oldVal) {
+      console.log(newVal, oldVal)
+    }
+  },
+  methods: {
+    format () {
+      this.formatName = this.name.split(' ').join('-').toUpperCase()
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-// import App from './App.vue'
-import Movie from './components/movies/movies.vue'
+import App from './App.vue'
+// import Movie from './components/movies/movies.vue'
 
 new Vue({
   el: '#app',
-  render: h => h(Movie)
+  render: h => h(App)
 })
